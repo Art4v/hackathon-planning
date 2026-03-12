8ba11ab14a6419d85a5fe7fdc6cb32d9a4a83df5
Flow69420 <sai.prakhya2008@gmail.com>
2026-03-12 12:02:08 +1100

    Revert "center dock updated"
    This reverts commit 4eabe87ca1e1b7274275c515526ccb7ff6f857b8.

--- ui_planning/components/Dock.jsx
+++ ui_planning/components/Dock.jsx
@@ -13,30 +13,27 @@ window.Dock = function Dock({ windows, openWindow }) {
 
   return (
     <div className="dock-wrap">
-      <div className="dock-cloud-shape"/>
-      <div className="dock-content">
-        <div className="mascot-wrap" ref={mascotRef}><CanaryLogo size={80}/></div>
-        <div className="dock-title">Canary <em>AI</em></div>
-        <div className="dock-tagline">The trader you always wanted to be</div>
-        <div className="dock-icons">
-          {DOCK_ITEMS.map(d => {
-            const isOpen = windows.find(w => w.id === d.id)?.open;
-            return (
-              <div key={d.id} className="dock-item" onClick={() => openWindow(d.id)}>
-                <div className="dock-btn"
-                  style={{ background: d.bg, borderColor: d.border, boxShadow: '0 5px 0 ' + d.shadow }}
-                  onMouseEnter={function(e) { AnimUtils.dockHoverIn(e.currentTarget); }}
-                  onMouseLeave={function(e) { AnimUtils.dockHoverOut(e.currentTarget); }}
-                  onMouseDown={function(e) { AnimUtils.dockPress(e.currentTarget); }}
-                  onMouseUp={function(e) { AnimUtils.dockRelease(e.currentTarget, d.shadow); }}>
-                  <iconify-icon icon={d.icon} width="32" style={{ color: d.iconColor }}/>
-                  <div className={'dock-dot ' + (isOpen ? 'visible' : '')}/>
-                </div>
-                <div className="dock-label">{d.label}</div>
+      <div className="mascot-wrap" ref={mascotRef}><CanaryLogo size={80}/></div>
+      <div className="dock-title">Canary <em>AI</em></div>
+      <div className="dock-tagline">The trader you always wanted to be</div>
+      <div className="dock-icons">
+        {DOCK_ITEMS.map(d => {
+          const isOpen = windows.find(w => w.id === d.id)?.open;
+          return (
+            <div key={d.id} className="dock-item" onClick={() => openWindow(d.id)}>
+              <div className="dock-btn"
+                style={{ background: d.bg, borderColor: d.border, boxShadow: '0 5px 0 ' + d.shadow }}
+                onMouseEnter={function(e) { AnimUtils.dockHoverIn(e.currentTarget); }}
+                onMouseLeave={function(e) { AnimUtils.dockHoverOut(e.currentTarget); }}
+                onMouseDown={function(e) { AnimUtils.dockPress(e.currentTarget); }}
+                onMouseUp={function(e) { AnimUtils.dockRelease(e.currentTarget, d.shadow); }}>
+                <iconify-icon icon={d.icon} width="32" style={{ color: d.iconColor }}/>
+                <div className={'dock-dot ' + (isOpen ? 'visible' : '')}/>
               </div>
-            );
-          })}
-        </div>
+              <div className="dock-label">{d.label}</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
